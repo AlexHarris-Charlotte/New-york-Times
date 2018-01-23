60fcfd7e84857da3de57480f2b271829be8e97bb
Alex Harris <akharri8@ncsu.edu>
2018-01-22 21:55:39 -0500

    Update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -17,28 +17,31 @@ $("#submit").on("click", function(event){
     var records = $("#records").val();
     var startYear = $("#startYear").val();
     var endYear = $("#endYear").val();
-
+    var searchYearStart = startYear + "0101";
+    var searchEndYear = endYear + "1231";
     var appendDiv = $("#append");
+    appendDiv.empty();
 
 
     var apiKey = "0e5cbc1fa8134deba3ce6e211baf64cf"
     // var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&page=" + records + "&begin_date=" + startYear + "&end_date=" + endYear + "&api-key=" + apiKey;
-    var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=" + apiKey;
+    var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&begin_date=" + searchYearStart + "&end_date=" + searchEndYear +  "&api-key=" + apiKey;
     console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET"
       }).done(function(response) {
         console.log(response);
-        var mainHeadline = response.response.docs[0].headline.main;
-        var author = response.response.docs[0].byline.person[0];
-        var section = response.response.docs[0].section_name;
-        var date = response.response.docs[0].pub_date;
-        var URL = response.response.docs[0].web_url;
+        for(var i = 0; i < records; i++) {
+        var mainHeadline = response.response.docs[i].headline.main;
+        var author = response.response.docs[i].byline.original;
+        var section = response.response.docs[i].section_name || "No Data Available";
+        var date = response.response.docs[i].pub_date;
+        var URL = response.response.docs[i].web_url;
         var newArticle = $("<div>");
         var urlLink = $('<a>');
         newArticle.append("<h1>"+ mainHeadline + "</h1>");
-        newArticle.append("<p>By: " + author + "</p>");
+        newArticle.append("<p>" + author + "</p>");
         newArticle.append("<p>"+section+"</p>");
         newArticle.append("<p>Published: " + date + "</p>");
         urlLink.attr('href', URL);
@@ -46,7 +49,8 @@ $("#submit").on("click", function(event){
         newArticle.append(urlLink);
         appendDiv.append(newArticle);
         console.log();
-    
+        }
+
       });
 });
 
