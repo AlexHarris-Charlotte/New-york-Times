2187b8abc6c0135dd4df0de0aa08f9a039f30f4e
Alex Harris <akharri8@ncsu.edu>
2018-01-22 20:59:13 -0500

    Created Query String

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,37 @@
-alert("hello");
+// API Key = 0e5cbc1fa8134deba3ce6e211baf64cf
+
+// On submit click
+    // Get the input value from the search term input
+    // Get the input value from number of records 
+    // Get the input value from start year 
+    // Get the input value from end year
+// store this value to a variable that we add into the query string
+// make the query string
+// use Ajax to call
+//
+
+
+$("#submit").on("click", function(event){
+    preventDefault(event);
+    var search = $("#searchTerm").val();
+    var records = $("#records").val();
+    var startYear = $("#startYear").val();
+    var endYear = $("#endYear").val();
+
+
+    var apiKey = "0e5cbc1fa8134deba3ce6e211baf64cf"
+    var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=" + search + "&page=" + records + "begin_date=" + startYear + "&end_date=" + endYear + "&api-key=" + apiKey;
+
+})
+
+
+
+    });
+    $.ajax({
+    url: url,
+    method: 'GET',
+    }).done(function(result) {
+    console.log(result);
+    }).fail(function(err) {
+    throw err;
+    });
